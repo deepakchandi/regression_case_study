ac0e5dde96e41f770957f609b87da2ef64b0ef64
Eric Bragas <ericbragas412@gmail.com>
2018-10-05 11:54:14 -0700

    Layout pipeline

--- src/model.py
+++ src/model.py
@@ -2,6 +2,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def test():
-    print('Yay!')
+# TODO: train test split? where?
 
+# TODO: Import data -> input: filepath -> numpy array
+def import_data(filepath):
+    return np.loadtxt(filepath)
+
+# NOTE: Feature selection: perform in notebook?
+
+# TODO: Feature clean-up -> input: x array -> cleaned x array
+# TODO: Create pipeline -> input: x array, y array -> pipeline
+# TODO: Grid search and cross-validate: input pipeline; return best hyper-parameters and score?
+    
