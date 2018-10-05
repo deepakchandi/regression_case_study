cfc467cdabb060425d49b1ec0a2afc9c82fe20e9
Eric Bragas <ericbragas412@gmail.com>
2018-10-05 12:37:02 -0700

    Add feature selection and cleaning functions

--- src/model.py
+++ src/model.py
@@ -4,13 +4,44 @@ import matplotlib.pyplot as plt
 
 # TODO: train test split? where?
 
-# TODO: Import data -> input: filepath -> numpy array
 def import_data(filepath):
-    return np.loadtxt(filepath)
+    """Read file from the path and return Pandas DataFrame"""
+    return pd.read_csv(filepath)
 
-# NOTE: Feature selection: perform in notebook?
+
+def select_features(df):
+    """Input the full dataset DataFrame and return features frame and taret frame."""
+    X_df = df[[ 'MachineID', 
+                'ModelID', 
+                'auctioneerID', 
+                'YearMade', 
+                'state',
+                'MachineHoursCurrentMeter']] # being called into question
+    
+    y_df = df['SalePrice']
+
+    return X_df, y_df
 
 # TODO: Feature clean-up -> input: x array -> cleaned x array
+# def feature_eng(x_df):
+#     x_df.get_dummies
+
+def clean_features(x_df):
+    """Input features DataFrame, clean features by columns, 
+    and return cleaned DataFrame"""
+    x_cleaned = x_df.copy()
+    
+    # State: dummy columns
+    states = pd.get_dummies(x_df['state'])
+    states.drop(['Unspecified', 'Washington DC'], axis=1, inplace=True)
+    x_cleaned.drop('state', axis=1, inplace=True)
+    x_cleaned = x_cleaned.merge(states, left_index=True, right_index=True)
+
+    return x_cleaned
+
+
 # TODO: Create pipeline -> input: x array, y array -> pipeline
+
+
 # TODO: Grid search and cross-validate: input pipeline; return best hyper-parameters and score?
     
